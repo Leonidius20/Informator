dabe1c6f9e3e3a468f7f72be1fc27fbd8f018cc8
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2020-07-16 17:17:58 +0300

    Improved code style, added a Maven dependency & updated README

--- README.md
+++ README.md
@@ -1,23 +1,17 @@
 # Informator
-**Informator** is a plugin for [NukkitX][nukkitx] server software for Minecraft Bedrock Edition, which allows you to customize join, leave and death messages. You can change their color and display type (display in chat, popup or not to display). This plugin also adds messages about bans and kicks. You can customize them too.
+**Informator** is a plugin for [NukkitX][nukkitx] server software for Minecraft Bedrock Edition, which allows customizing messages displayed when a player joins the game, leaves it or dies. The plugin also adds customizable messages about players getting banned or kicked. The customizable properties of messages include the color and the way of displaying (in the chat, as a popup or not displaying at all).
 
 ### Configuration file
-* _join_: chat/popup/none (how to display join message)
-* _join-color_: [character] (the color of join message)
-* _quit_: chat/popup/none
-* _quit-color_: [character]
-* _death_: chat/popup/none
-* _death-color_: [character]
-* _ban_: chat/popup/none
-* _ban-color_: [character]
-* _kick_: chat/popup/none
-* _kick-color_: [character]
+_join_, _quit_, _death_, _ban_, _kick_ parameters define the type of message to be displayed when corresponding events happen. Their values can be set to _chat_ (send the message to chat, which is the default option), _popup_ (the message will be displayed as a popup above the hotbar) or _none_ (the message will not be displayed at all).
+
+_join-color_, _quit-color_, _death-color_, _ban-color_, _kick-color_ parameters define the colors of corresponding messages. Their values can be set to [Minecraft color codes][colors] (a **0-9** digit or an **a-f** letter).
   
 ### Building from sources
-In order to build the plugin from sources, you must add [NukkitX][nukkitx] as a library.
+`mvn clean package`
 
 ### Links
 * [Informator on nukkitx.com](https://nukkitx.com/resources/informator.32/)
 * [Informator on nukkit.ru](http://forums.voxelwind.com/resources/informator.121/)
 
 [nukkitx]: http://github.com/NukkitX/Nukkit
+[colors]: https://minecraft.gamepedia.com/Formatting_codes#Color_codes
--- src/main/java/ua/leonidius/informator/Main.java
+++ src/main/java/ua/leonidius/informator/Main.java
@@ -12,117 +12,111 @@ import cn.nukkit.lang.TranslationContainer;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.TextFormat;
 
-/**
- * Created by lion on 12.03.17.
- */
-public class Main extends PluginBase implements Listener{
+public class Main extends PluginBase implements Listener {
 
-
-    private String jointype;
-    private String quittype;
-    private String deathtype;
-    private String bantype;
-    private String kicktype;
-    private char joincolor;
-    private char quitcolor;
-    private char deathcolor;
-    private char bancolor;
-    private char kickcolor;
+    private String joinType, quitType, deathType, banType, kickType;
+    private char joinColor, quitColor, deathColor, banColor, kickColor;
 
     @Override
-    public void onEnable(){
+    public void onEnable() {
         initCfg();
         loadCfg();
         this.getServer().getPluginManager().registerEvents(this, this);
     }
 
-    private void initCfg(){
+    private void initCfg() {
         this.getDataFolder().mkdirs();
         this.saveResource("config.yml");
     }
 
-    private void loadCfg(){
-        jointype = this.getConfig().getString("join");
-        joincolor = this.getConfig().getString("join-color").charAt(0);
-        quittype = this.getConfig().getString("quit");
-        quitcolor = this.getConfig().getString("quit-color").charAt(0);
-        deathtype = this.getConfig().getString("death");
-        deathcolor = this.getConfig().getString("death-color").charAt(0);
-        bantype = this.getConfig().getString("ban");
-        bancolor = this.getConfig().getString("ban-color").charAt(0);
-        kicktype = this.getConfig().getString("kick");
-        kickcolor = this.getConfig().getString("kick-color").charAt(0);
+    private void loadCfg() {
+        joinType = this.getConfig().getString("join");
+        joinColor = this.getConfig().getString("join-color").charAt(0);
+        quitType = this.getConfig().getString("quit");
+        quitColor = this.getConfig().getString("quit-color").charAt(0);
+        deathType = this.getConfig().getString("death");
+        deathColor = this.getConfig().getString("death-color").charAt(0);
+        banType = this.getConfig().getString("ban");
+        banColor = this.getConfig().getString("ban-color").charAt(0);
+        kickType = this.getConfig().getString("kick");
+        kickColor = this.getConfig().getString("kick-color").charAt(0);
     }
 
-    private String textFromContainer(TextContainer container){
-        if (container instanceof TranslationContainer){
-            return this.getServer().getLanguage().translateString(container.getText(), ((TranslationContainer)container).getParameters());
+    private String textFromContainer(TextContainer container) {
+        if (container instanceof TranslationContainer) {
+            return this.getServer().getLanguage().translateString(container.getText(), ((TranslationContainer) container).getParameters());
         } else {
             return container.getText();
         }
     }
 
+    @SuppressWarnings("unused")
     @EventHandler
-    public void onPlayerJoin (PlayerJoinEvent e){
+    public void onPlayerJoin(PlayerJoinEvent e) {
         String msg = TextFormat.clean(this.textFromContainer(e.getJoinMessage()));
-        String output = TextFormat.colorize("&"+joincolor+msg);
-        if (jointype.equalsIgnoreCase("popup")) {
+        String output = TextFormat.colorize("&" + joinColor + msg);
+        if (joinType.equalsIgnoreCase("popup")) {
             e.setJoinMessage("");
             for (Player onlinePlayer : this.getServer().getOnlinePlayers().values()) onlinePlayer.sendPopup(output);
-        } else if (jointype.equalsIgnoreCase("none")){
+        } else if (joinType.equalsIgnoreCase("none")) {
             e.setJoinMessage("");
-        } else if (jointype.equalsIgnoreCase("chat")){
+        } else if (joinType.equalsIgnoreCase("chat")) {
             e.setJoinMessage(output);
         }
     }
 
+    @SuppressWarnings("unused")
     @EventHandler
-    public void onPlayerQuit (PlayerQuitEvent e){
+    public void onPlayerQuit(PlayerQuitEvent e) {
         String msg = TextFormat.clean(this.textFromContainer(e.getQuitMessage()));
-        String output = TextFormat.colorize("&"+quitcolor+msg);
-        if (jointype.equalsIgnoreCase("popup")) {
+        String output = TextFormat.colorize("&" + quitColor + msg);
+        if (joinType.equalsIgnoreCase("popup")) {
             e.setQuitMessage("");
             for (Player onlinePlayer : this.getServer().getOnlinePlayers().values()) onlinePlayer.sendPopup(output);
-        } else if (quittype.equalsIgnoreCase("none")){
+        } else if (quitType.equalsIgnoreCase("none")) {
             e.setQuitMessage("");
-        } else if (quittype.equalsIgnoreCase("chat")){
+        } else if (quitType.equalsIgnoreCase("chat")) {
             e.setQuitMessage(output);
         }
     }
 
+    @SuppressWarnings("unused")
     @EventHandler
-    public void onPlayerDeath (PlayerDeathEvent e){
+    public void onPlayerDeath(PlayerDeathEvent e) {
         String msg = TextFormat.clean(this.textFromContainer(e.getDeathMessage()));
-        String output = TextFormat.colorize("&"+deathcolor+msg);
-        if (deathtype.equalsIgnoreCase("popup")) {
+        String output = TextFormat.colorize("&" + deathColor + msg);
+        if (deathType.equalsIgnoreCase("popup")) {
             e.setDeathMessage("");
             for (Player onlinePlayer : this.getServer().getOnlinePlayers().values()) onlinePlayer.sendPopup(output);
-        } else if (deathtype.equalsIgnoreCase("none")){
+        } else if (deathType.equalsIgnoreCase("none")) {
             e.setDeathMessage("");
-        } else if (deathtype.equalsIgnoreCase("chat")){
+        } else if (deathType.equalsIgnoreCase("chat")) {
             e.setDeathMessage(output);
         }
     }
 
+    @SuppressWarnings("unused")
     @EventHandler
-    public void onKick (PlayerKickEvent e){
-        if(e.getReasonEnum() == PlayerKickEvent.Reason.NAME_BANNED || e.getReasonEnum() == PlayerKickEvent.Reason.IP_BANNED){
-            TranslationContainer baninfo = new TranslationContainer("%commands.ban.success", e.getPlayer().getName());
-            String msg = this.textFromContainer(baninfo);
-            String output = TextFormat.colorize("&"+bancolor+msg);
-            if (bantype.equalsIgnoreCase("popup")) {
+    public void onKick(PlayerKickEvent e) {
+        if (e.getReasonEnum() == PlayerKickEvent.Reason.NAME_BANNED || e.getReasonEnum() == PlayerKickEvent.Reason.IP_BANNED) {
+            TranslationContainer banInfo = new TranslationContainer("%commands.ban.success", e.getPlayer().getName());
+            String msg = this.textFromContainer(banInfo);
+            String output = TextFormat.colorize("&" + banColor + msg);
+            if (banType.equalsIgnoreCase("popup")) {
                 for (Player onlinePlayer : this.getServer().getOnlinePlayers().values()) onlinePlayer.sendTip(output);
-            } else if (bantype.equalsIgnoreCase("chat")){
-                for (Player onlinePlayer : this.getServer().getOnlinePlayers().values()) onlinePlayer.sendMessage(output);
+            } else if (banType.equalsIgnoreCase("chat")) {
+                for (Player onlinePlayer : this.getServer().getOnlinePlayers().values())
+                    onlinePlayer.sendMessage(output);
             }
-        } else if (e.getReasonEnum() == PlayerKickEvent.Reason.KICKED_BY_ADMIN){
-            TranslationContainer baninfo = new TranslationContainer("%commands.kick.success", e.getPlayer().getName());
-            String msg = this.textFromContainer(baninfo);
-            String output = TextFormat.colorize("&"+kickcolor+msg);
-            if (kicktype.equalsIgnoreCase("popup")) {
+        } else if (e.getReasonEnum() == PlayerKickEvent.Reason.KICKED_BY_ADMIN) {
+            TranslationContainer banInfo = new TranslationContainer("%commands.kick.success", e.getPlayer().getName());
+            String msg = this.textFromContainer(banInfo);
+            String output = TextFormat.colorize("&" + kickColor + msg);
+            if (kickType.equalsIgnoreCase("popup")) {
                 for (Player onlinePlayer : this.getServer().getOnlinePlayers().values()) onlinePlayer.sendTip(output);
-            } else if (kicktype.equalsIgnoreCase("chat")){
-                for (Player onlinePlayer : this.getServer().getOnlinePlayers().values()) onlinePlayer.sendMessage(output);
+            } else if (kickType.equalsIgnoreCase("chat")) {
+                for (Player onlinePlayer : this.getServer().getOnlinePlayers().values())
+                    onlinePlayer.sendMessage(output);
             }
         }
     }
